caeab27c2394bd507ed67f46730c61f17c42f58f
danevputra <73149844+danevputra@users.noreply.github.com>
2020-12-04 00:30:26 +0700

    Add files via upload

--- navbar1.php
+++ navbar1.php
@@ -1,3 +1,19 @@
-<a href="login.php">Login</a>
+<!--<a href="login.php">Login</a>
 <a href="signup.php">Sign Up</a>
-<br/>
+<br/>-->
+
+<body>
+<nav class="navbar navbar-expand-sm bg-primary">
+    <a class="navbar-brand" href="#">
+        <img src="view/img/logo.png" width="30" height="30" alt="">
+        <span style="color:white;">Escalate</span>
+    </a>
+    <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link" style="color:white;" href="login.php">Login</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" style="color:white;" href="signup.php">Sign Up</a>
+    </li>
+    </ul> 
+</nav>
